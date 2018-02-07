6abbaac59e297cbe69020f2c2bc0a5790c6487d8
CHIEWADA\千恵 <chie.w0529@gmailcom>
2018-02-07 12:12:17 +0900

    4-5

--- Practice/src/practice04/PTra04_05.java
+++ Practice/src/practice04/PTra04_05.java
@@ -47,15 +47,28 @@ public class PTra04_05 {
 			if (cpuHand == 0) {
 				System.out.println("グー！");
 
+				if(myHand == 2) {
+					System.out.println("あなたの勝ち");
+					winFlg = true;
+				}
 
 
 			} else if (cpuHand == 1) {
 				System.out.println("チョキ！！");
 
+				if(myHand == 0) {
+					System.out.println("あなたの勝ち");
+					winFlg = true;
+				}
+
 
 
 			} else {
 				System.out.println("パー！！");
+				if(myHand == 1) {
+					System.out.println("あなたの勝ち");
+					winFlg = true;
+				}
 
 
 
